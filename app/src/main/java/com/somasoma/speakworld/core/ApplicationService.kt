cd2194e209eeaad5f6011b852cc3435ca9c33cd4
SeungHyun Lee <shlee4290@naver.com>
2021-09-03 23:23:46 +0900

    firebase realtime DB에 현재 유저 접속 상태 저장하는 로직 추가 - 1

---
+++ app/src/main/java/com/somasoma/speakworld/core/ApplicationService.kt
@@ -0,0 +1,30 @@
+package com.somasoma.speakworld.core
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import com.somasoma.speakworld.core.usecase.UserConnectedStateUseCase
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class ApplicationService : Service() {
+
+    @Inject
+    lateinit var userConnectedStateUseCase: UserConnectedStateUseCase
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        userConnectedStateUseCase.postCurrentUserOffline()
+
+        super.onTaskRemoved(rootIntent)
+        stopSelf()
+    }
+}
