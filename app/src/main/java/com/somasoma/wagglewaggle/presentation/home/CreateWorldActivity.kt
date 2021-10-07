4ccd1199034b9170524a252b0abcd6b4a5d79a33
SeungHyun Lee <shlee4290@naver.com>
2021-10-07 13:34:22 +0900

    방만들기 화면 레이아웃 수정

--- app/src/main/java/com/somasoma/wagglewaggle/presentation/home/CreateWorldActivity.kt
+++ app/src/main/java/com/somasoma/wagglewaggle/presentation/home/CreateWorldActivity.kt
@@ -2,9 +2,16 @@ package com.somasoma.wagglewaggle.presentation.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.somasoma.wagglewaggle.R
+import com.somasoma.wagglewaggle.databinding.ActivityCreateWorldBinding
 
 class CreateWorldActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreateWorldBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_create_world)
     }
 }
