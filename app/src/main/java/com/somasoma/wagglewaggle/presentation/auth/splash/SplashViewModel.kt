a41a143a13300942f2f6663e380130590ee7f21d
SeungHyun Lee <shlee4290@naver.com>
2021-10-09 13:40:08 +0900

    스플래시 화면 추가

---
+++ app/src/main/java/com/somasoma/wagglewaggle/presentation/auth/splash/SplashViewModel.kt
@@ -0,0 +1,90 @@
+package com.somasoma.wagglewaggle.presentation.auth.splash
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.somasoma.wagglewaggle.core.NetworkUtil
+import com.somasoma.wagglewaggle.core.PreferenceConstant
+import com.somasoma.wagglewaggle.core.SharedPreferenceHelper
+import com.somasoma.wagglewaggle.core.SingleLiveEvent
+import com.somasoma.wagglewaggle.data.model.dto.auth.FirebaseRequest
+import com.somasoma.wagglewaggle.domain.usecase.auth.PostFirebaseUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.disposables.CompositeDisposable
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+    application: Application,
+    private val sharedPreferenceHelper: SharedPreferenceHelper,
+    private val networkUtil: NetworkUtil,
+    private val postFirebaseUseCase: PostFirebaseUseCase
+) : AndroidViewModel(application) {
+
+    val navigateToSignInAndSignUpEvent = SingleLiveEvent<Unit>()
+    val navigateToMainEvent = SingleLiveEvent<Unit>()
+    var firebaseUserToken: String = ""
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+
+    fun getAccessToken() {
+        networkUtil.publicRestApiCall(
+            postFirebaseUseCase.postFirebase(
+                FirebaseRequest(
+                    firebaseUserToken
+                )
+            ),
+            compositeDisposable
+        ) {
+            onSuccessCallback = {
+                it?.run {
+                    accessToken?.let {
+                        sharedPreferenceHelper.putString(
+                            PreferenceConstant.ACCESS_TOKEN,
+                            accessToken
+                        )
+                    }
+
+                    accessTokenExpiresIn?.let {
+                        sharedPreferenceHelper.putLong(
+                            PreferenceConstant.ACCESS_TOKEN_EXPIRED_IN,
+                            accessTokenExpiresIn
+                        )
+                    }
+
+                    refreshToken?.let {
+                        sharedPreferenceHelper.putString(
+                            PreferenceConstant.REFRESH_TOKEN,
+                            refreshToken
+                        )
+                    }
+
+                    isNewMember?.let {
+                        if (isNewMember == "y") {
+                            navigateToSignInAndSignUpEvent.call()
+                        } else {
+                            navigateToMainEvent.call()
+                        }
+                    }
+                }
+
+
+            }
+
+            onErrorCallback = {
+
+            }
+
+            onNetworkErrorCallback = {
+
+            }
+
+            onFinishCallback = {
+
+            }
+        }
+    }
+}
