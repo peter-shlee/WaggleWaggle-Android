c433a1ae92a36b1e13af884c4bb15d7db94a6070
SeungHyun Lee <shlee4290@naver.com>
2021-10-13 18:24:44 +0900

    프로필 수정 일부 구현

--- app/src/main/java/com/somasoma/wagglewaggle/presentation/setting/EditProfileViewModel.kt
+++ app/src/main/java/com/somasoma/wagglewaggle/presentation/setting/EditProfileViewModel.kt
@@ -4,13 +4,17 @@ import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.somasoma.wagglewaggle.core.InputState
 import com.somasoma.wagglewaggle.core.NetworkUtil
 import com.somasoma.wagglewaggle.core.SingleLiveEvent
+import com.somasoma.wagglewaggle.data.model.dto.member.Member
 import com.somasoma.wagglewaggle.domain.usecase.member.GetCountryListUseCase
 import com.somasoma.wagglewaggle.domain.usecase.member.GetInterestListUseCase
 import com.somasoma.wagglewaggle.domain.usecase.member.GetLanguageListUseCase
+import com.somasoma.wagglewaggle.domain.usecase.member.PutEditMemberUseCase
 import com.somasoma.wagglewaggle.presentation.custom_views.SelectInterestsViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.util.regex.Pattern
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,8 +23,13 @@ class EditProfileViewModel @Inject constructor(
     private val networkUtil: NetworkUtil,
     private val getLanguageListUseCase: GetLanguageListUseCase,
     private val getCountryListUseCase: GetCountryListUseCase,
+    private val putEditMemberUseCase: PutEditMemberUseCase,
     getInterestListUseCase: GetInterestListUseCase
 ) : SelectInterestsViewModel(application, networkUtil, getInterestListUseCase) {
+    companion object {
+        private const val NICKNAME_REGEX = "[A-Za-z|가-힣|ㄱ-ㅎ|ㅏ-ㅣ\\d\\s]{2,8}\$"
+        private val nicknamePattern = Pattern.compile(NICKNAME_REGEX)
+    }
 
     val showSelectInterestsDialogEvent = SingleLiveEvent<Unit>()
     val navigateToPrevPageEvent = SingleLiveEvent<Unit>()
@@ -30,12 +39,17 @@ class EditProfileViewModel @Inject constructor(
     val languages: LiveData<List<String?>> = _languages
     var selectedLanguage: String? = null
     var selectedCountry: String? = null
+    private val _nicknameInputState = MutableLiveData(InputState.DISABLED)
+    val nicknameInputState: LiveData<InputState> = _nicknameInputState
+    private val _showDuplicateNicknameText = MutableLiveData(false)
+    val showDuplicateNicknameText: LiveData<Boolean> = _showDuplicateNicknameText
+    private var nickname: String = ""
+    var introduceMySelf: CharSequence = ""
 
     init {
         getLanguageList()
         getCountryList()
-
-        resetSelectedInterests(setOf("스포츠", "BTS"))
+        resetSelectedInterests(setOf())
     }
 
     fun onClickBackButton() {
@@ -44,6 +58,53 @@ class EditProfileViewModel @Inject constructor(
 
     fun onClickSelectInterestButton() {
         showSelectInterestsDialogEvent.call()
+    }
+
+    fun onClickCheckNicknameDuplicatedButton() {
+        _nicknameInputState.value = InputState.POSITIVE
+    }
+
+    fun onClickSaveButton() {
+        networkUtil.restApiCall(
+            putEditMemberUseCase::putEditMember, Member(
+                null,
+                nickname,
+                selectedCountry,
+                selectedLanguage,
+                introduceMySelf.toString(),
+                null,
+                null,
+                null,
+                null,
+                null
+            ), viewModelScope
+        ) {
+
+        }
+    }
+
+    fun onIntroduceChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+        introduceMySelf = s
+    }
+
+    fun onNicknameChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+        _nicknameInputState.value = InputState.DISABLED
+        _showDuplicateNicknameText.value = false
+        nickname = s.toString().trim()
+        validateNickname(nickname)
+    }
+
+    private fun validateNickname(nickname: String) {
+        if (nickname.isBlank()) {
+            _nicknameInputState.value = InputState.DISABLED
+            return
+        }
+
+        if (nicknamePattern.matcher(nickname).matches()) {
+            _nicknameInputState.value = InputState.ENABLED
+        } else {
+            _nicknameInputState.value = InputState.NEGATIVE
+        }
     }
 
     private fun getCountryList() {
